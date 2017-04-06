a724d7dc6d9d0ef003cdb692291593272bb0dc3d
wilfriedbobo <wilfried.nguessan.bobo@gmail.com>
2017-04-06 12:16:30 +0200

    structure tablette

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+var app = angular.module("app", []);
+    app.controller('myCtrl', ['$scope', '$http', function ($scope, $http) {
+      $http.get("assets/json/pads.json").then(function(toto) {
+        $scope.tabs = toto.data;
+      });
+    }])
