0d3727b9a6441b709217a2dfb76db8dd7bc8ce8c
JonathanMoroy <jonathan.moroy@gmail.com>
2017-04-06 12:19:01 +0200

    Ajout des routes d'une nav archaïque et du json watch rempli et afficher dans la vue watch.html avec le ng repeat et un style de base

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,50 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var App = angular.module('App', ['ngRoute']);
+        
 
+App.config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+                .when('/computers', {
+                    templateUrl: 'assets/partials/computers.html',
+                    controller: 'computersCtrl'
+                })
+                .when('/phones', {
+                    templateUrl: 'assets/partials/phones.html',
+                    controller: 'phonesCtrl'
+                })
+                .when('/pads', {
+                    templateUrl: 'assets/partials/pads.html',
+                    controller: 'padsCtrl'
+                })
+                .when('/watchs', {
+                    templateUrl: 'assets/partials/watchs.html',
+                    controller: 'watchsCtrl'
+                })
+                .otherwise({
+                    redirectTo: '/computers'
+                });
+    }]);
 
+App.controller('indexCtrl', ['$scope', function ($scope) {
+        // Configuration de mon contrôleur index
+     
+    }]);
+
+App.controller('computersCtrl', ['$scope', function ($scope) {
+        // Configuration de mon contrôleur home
+    }]);
+
+App.controller('phonesCtrl', ['$scope', function ($scope) {
+        // Configuration de mon contrôleur resume
+    }]);
+
+App.controller('padsCtrl', ['$scope', function ($scope) {
+        // Configuration de mon contrôleur contact
+    }]);
+
+App.controller('watchsCtrl', ['$scope', '$http', function ($scope, $http) {
+        // Configuration de mon contrôleur project
+        $http.get('assets/json/watchs.json')
+            .then(function (res) {
+                $scope.watchs = res.data;
+            });
+    }]);
