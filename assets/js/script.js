4717419d599c3d16783f018d5380acb71560d6f4
Valerie Jardin <valerie.jardin60@gmail.com>
2017-04-06 12:16:39 +0200

    Index json et script opérationnels

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,13 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// On déclare le module
+    var app = angular.module('computersApp', []);
+    // On défini le contrôleur
+    app.controller('computersCtrl', function($scope, $http) {
+      // On va chercher le contenu de computers.json
+      $http.get('assets/json/computers.json')
+        // Si on le récupère avec succès on le stocke dans la variable $scope.computers
+        .then(function(res){
+          $scope.computers = res.data;
+        });
+    });
 
 
