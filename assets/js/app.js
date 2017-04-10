e21d367ba2904640d8ed247556aaa4e3d10f0d07
wilfriedbobo <wilfried.nguessan.bobo@gmail.com>
2017-04-10 12:20:26 +0200

    inscription/connexion

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,11 +27,47 @@ App.config(['$routeProvider', function ($routeProvider) {
                 });
     }]);
 
-App.controller('indexCtrl', ['$scope', function ($scope) {
+App.controller('indexCtrl', ['$scope', '$http', function ($scope, $http) {
         // Configuration de mon contrôleur index
         $scope.remove = function ($index) {
           $scope.carts.splice(this.$index, 1);
       };
+      // Configuration Regex
+      $scope.username = {
+            regex: /^[A-z- \\áàâäãçéèêëíìîïñóòôöõúùûüÁÀÂÄÇÉÈÊËÍÌÎÏÓÒÔÖÚÙÛÜ\s]{1,90}$/
+        };
+        $scope.email = {
+            regex: /^[\w\-\.]+[a-z0-9]@[\w\-\.]+[a-z0-9]\.[a-z]{2,}$/
+          };
+          $scope.someText = {
+            regex: /^[\d\D]{1,}$/,
+            model: 'Entrez votre texte'
+          };
+        // Configuration de l'inscription
+        $scope.envoie_donnee = function () {
+          var data = {
+            nom: $scope.nom,
+            prenom: $scope.prenom,
+            date_de_naissance: $scope.birthday,
+            adresse_e_mail: $scope.e_mail,
+            mot_de_passe: $scope.password
+          };
+          var config = {
+                headers : {
+                    'Content-Type': 'assets/json/inscription;'
+                }
+            }
+          $http.post('assets/json/inscription', data)
+            .then(function (data) {
+                $scope.inscription = data.data;
+            })
+            .error(function (data, status, header, config) {
+                $scope.ResponseDetails = "Data: " + data +
+                    "<hr />status: " + status +
+                    "<hr />headers: " + header +
+                    "<hr />config: " + config;
+            })
+        };
     }]);
 
 App.controller('computersCtrl', ['$scope', '$http', function ($scope, $http) {
